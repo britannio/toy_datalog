ffa16c9bcd3089f02860555d3aff073c8131a937
Britannio <britanniojarrett@gmail.com>
2023-08-28 21:41:55 +0100

    complete implementation

--- lib/toy_datalog.dart
+++ lib/toy_datalog.dart
@@ -1,24 +1,22 @@
 import 'package:collection/collection.dart';
 
 typedef Query = ({List<String> find, List<Triple> where});
-typedef Triple = (dynamic, String, dynamic);
-typedef Context = Map<String, dynamic>;
+typedef Triple = (Object, String, Object);
+typedef Context = Map<String, Object>;
 
-void demo() {
-  final Query query = (
-    find: [],
-    where: [],
-  );
+extension TripleX on Triple {
+  Object operator [](int index) => values[index];
+
+  List<Object> get values => [$1, $2, $3];
 }
 
-Context? matchPattern(Triple pattern, Triple triple, Context context) {
-  final $pattern = [pattern.$1, pattern.$2, pattern.$3];
+Context? matchPattern(Triple pattern, Triple triple, Context ctx) {
   // Starting with the original context, compare index 1-3 of the pattern vs
   // triple and build up the context. If there is a failed match then the
   // context becomes empty.
-  return $pattern.foldIndexed(context, (index, context, patternPart) {
-    final $triple = [triple.$1, triple.$2, triple.$3][index];
-    return matchPart(patternPart, $triple, context);
+
+  return pattern.values.foldIndexed(ctx, (index, context, patternPart) {
+    return matchPart(patternPart, triple[index], context);
   });
 }
 
@@ -37,10 +35,36 @@ Context? matchPart(Object patternPart, Object triplePart, Context? context) {
 Context? matchVariable(String variable, Object triplePart, Context context) {
   if (context.containsKey(variable)) {
     // The variable has a value in the context
-    final bound = context[variable];
+    final bound = context[variable]!;
     // Populate context by now searching with this added restriction
     return matchPart(bound, triplePart, context);
   } else {
-    return context..[variable] = triplePart;
+    return {...context, variable: triplePart};
   }
+}
+
+List<Context> querySingle(Triple pattern, List<Triple> db, Context ctx) {
+  return db.map((t) => matchPattern(pattern, t, ctx)).whereNotNull().toList();
+}
+
+List<Context> queryWhere(List<Triple> patterns, List<Triple> db) {
+  return patterns.fold([{}], (contexts, pattern) {
+    return contexts
+        .map((ctx) => querySingle(pattern, db, ctx))
+        .flattened
+        .toList();
+  });
+}
+
+List<Object> query(Query query, List<Triple> db) {
+  final List<Context> contexts = queryWhere(query.where, db);
+  return contexts.map((ctx) => actualize(ctx, query.find)).toList();
+}
+
+List<Object> actualize(Context ctx, List<String> find) {
+  // find may contain constants, otherwise the variable is looked up.
+  // Can the variable lookup fail?
+  return find
+      .map((findPart) => isVariable(findPart) ? ctx[findPart]! : findPart)
+      .toList();
 }
--- test/toy_datalog_test.dart
+++ test/toy_datalog_test.dart
@@ -1,8 +1,9 @@
+import 'package:toy_datalog/example_triples.dart';
 import 'package:toy_datalog/toy_datalog.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('matchPattern positive', () {
+  test('matchPattern() positive', () {
     expect(
       matchPattern(
         ("?movieId", "movie/director", "?directorId"),
@@ -22,5 +23,50 @@ void main() {
       ),
       isNull,
     );
+  });
+
+  test('querySingle()', () {
+    expect(
+      querySingle(("?movieId", "movie/year", 1987), exampleTriples, {}),
+      [
+        {"?movieId": 202},
+        {"?movieId": 203},
+        {"?movieId": 204}
+      ],
+    );
+  });
+
+  test('queryWhere()', () {
+    expect(
+      queryWhere(
+        [
+          ("?movieId", "movie/title", "The Terminator"),
+          ("?movieId", "movie/director", "?directorId"),
+          ("?directorId", "person/name", "?directorName"),
+        ],
+        exampleTriples,
+      ),
+      [
+        {"?movieId": 200, "?directorId": 100, "?directorName": "James Cameron"},
+      ],
+    );
+  });
+
+  test('where & find', () {
+    expect(
+        query(
+          (
+            find: ["?directorName"],
+            where: [
+              ("?movieId", "movie/title", "The Terminator"),
+              ("?movieId", "movie/director", "?directorId"),
+              ("?directorId", "person/name", "?directorName"),
+            ],
+          ),
+          exampleTriples,
+        ),
+        [
+          ["James Cameron"]
+        ]);
   });
 }
